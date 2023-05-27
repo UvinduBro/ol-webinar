8cfc589c12b2f5e5a9f41b65e7cd037aa1bb4cfa
Uvindu Rajapakshe <118661330+UvinduBro@users.noreply.github.com>
2023-05-27 20:30:41 +0000

    new file:   countdown.css 	new file:   index.html 	new file:   script.js 	new file:   styles.css 	new file:   wrapper.css

---
+++ script.js
@@ -0,0 +1,45 @@
+// Function to handle the conference button click
+function joinConferenceCall() {
+    // Add your code here to join the conference call
+    window.location.href = "https://example.com/conference-call"; // Replace with your conference call link
+  }
+  
+  // Function to handle the WhatsApp button click
+  function joinWhatsAppGroup() {
+    // Add your code here to join the WhatsApp group
+    window.location.href = "https://example.com/whatsapp-group"; // Replace with your WhatsApp group link
+  }
+  
+  // Attach click event listeners to the buttons
+  document.getElementById("conferenceButton").addEventListener("click", joinConferenceCall);
+  document.getElementById("whatsappButton").addEventListener("click", joinWhatsAppGroup);
+
+  
+  // count down
+
+  function countdown() {
+    const endDate = new Date("June 8, 2023 00:00:00").getTime();
+    const now = new Date().getTime();
+    const timeLeft = endDate - now;
+  
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+  
+    if (timeLeft < 0) {
+      clearInterval(countdownInterval);
+      document.getElementById("days").innerHTML = "0";
+      document.getElementById("hours").innerHTML = "0";
+      document.getElementById("minutes").innerHTML = "0";
+      document.getElementById("seconds").innerHTML = "0";
+    }
+  }
+  
+  const countdownInterval = setInterval(countdown, 1000);
+  
